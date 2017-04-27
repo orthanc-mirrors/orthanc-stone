b0e7a48066dc1236a511fd8b516fe159346cbb51
Sebastien Jodogne <s.jodogne@gmail.com>
2017-04-27 11:00:15 +0200

    agpl license

--- Resources/CMake/SdlConfiguration.cmake
+++ Resources/CMake/SdlConfiguration.cmake
@@ -1,3 +1,22 @@
+# Stone of Orthanc
+# Copyright (C) 2012-2016 Sebastien Jodogne, Medical Physics
+# Department, University Hospital of Liege, Belgium
+# Copyright (C) 2017 Osimis, Belgium
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 if (STATIC_BUILD OR NOT USE_SYSTEM_SDL)
   SET(SDL_SOURCES_DIR ${CMAKE_BINARY_DIR}/SDL2-2.0.4)
   SET(SDL_URL "http://www.orthanc-server.com/downloads/third-party/Stone/SDL2-2.0.4.tar.gz")

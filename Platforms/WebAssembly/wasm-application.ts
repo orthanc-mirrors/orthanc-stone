6493bf937129dac907671c82e12656d4aa07aa68
am@osimis.io <am@osimis.io>
2018-06-14 11:22:03 +0200

    fix
    --HG--
    branch : am

---
+++ Platforms/WebAssembly/wasm-application.ts
@@ -0,0 +1,10 @@
+///<reference path='stone-framework-loader.ts'/>
+
+declare function InitializeWasmApplication() :void; // still in a js file
+
+
+// Wait for the Orthanc Framework to be initialized (this initializes
+// the WebAssembly environment)
+Stone.Framework.Initialize(true, function() {
+    InitializeWasmApplication();
+});

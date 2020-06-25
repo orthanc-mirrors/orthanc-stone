2560ebc8fb0b50d92703ded4e2d8529ec42b7af0
Sebastien Jodogne <s.jodogne@gmail.com>
2020-06-25 09:13:56 +0200

    Building WebAssembly samples using Docker

---
+++ Samples/WebAssembly/docker-internal.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -ex
+
+source /opt/emsdk/emsdk_env.sh
+
+# Use a folder that is writeable by non-root users for the Emscripten cache
+export EM_CACHE=/tmp/emscripten-cache
+
+# Get the Orthanc framework
+cd /tmp/
+hg clone https://hg.orthanc-server.com/orthanc/
+
+# Make a copy of the source code in a writeable folder, because of
+# "DownloadPackage.cmake" that writes to the "ThirdPartyDownloads"
+# folder next to the "CMakeLists.txt"
+cd /source
+hg archive /tmp/source-writeable
+
+mkdir /tmp/build
+cd /tmp/build
+
+cmake /tmp/source-writeable/Samples/WebAssembly \
+      -DCMAKE_BUILD_TYPE=$1 \
+      -DCMAKE_INSTALL_PREFIX=/target \
+      -DCMAKE_TOOLCHAIN_FILE=${EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
+      -DORTHANC_FRAMEWORK_ROOT=/tmp/orthanc \
+      -DSTATIC_BUILD=ON \
+      -G Ninja
+
+ninja -j2 install

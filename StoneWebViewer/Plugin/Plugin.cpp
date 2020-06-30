f6a045809205a3d22df74197f7802c8d21d60d97
Sebastien Jodogne <s.jodogne@gmail.com>
2020-06-30 10:29:10 +0200

    improved Orthanc::Logging::Initialize()

--- StoneWebViewer/Plugin/Plugin.cpp
+++ StoneWebViewer/Plugin/Plugin.cpp
@@ -163,7 +163,12 @@ extern "C"
   ORTHANC_PLUGINS_API int32_t OrthancPluginInitialize(OrthancPluginContext* context)
   {
     OrthancPlugins::SetGlobalContext(context);
+
+#if defined(ORTHANC_FRAMEWORK_VERSION_IS_ABOVE)  // This indicates Orthanc framework >= 1.7.2
     Orthanc::Logging::InitializePluginContext(context);
+#else
+    Orthanc::Logging::Initialize(context);
+#endif
 
     /* Check the version of the Orthanc core */
     if (OrthancPluginCheckVersion(context) == 0)

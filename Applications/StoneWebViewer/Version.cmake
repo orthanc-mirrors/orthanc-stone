f106011ef642ff15f88e08c512eb3af659d73b8c
Sebastien Jodogne <s.jodogne@gmail.com>
2022-03-24 09:08:51 +0100

    StoneWebViewer-2.3
    --HG--
    branch : StoneWebViewer-2.3

--- Applications/StoneWebViewer/Version.cmake
+++ Applications/StoneWebViewer/Version.cmake
@@ -18,12 +18,12 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
-set(STONE_WEB_VIEWER_VERSION "mainline")
+set(STONE_WEB_VIEWER_VERSION "2.3")
 
 if (STONE_WEB_VIEWER_VERSION STREQUAL "mainline")
   set(ORTHANC_FRAMEWORK_DEFAULT_VERSION "mainline")
   set(ORTHANC_FRAMEWORK_DEFAULT_SOURCE "hg")
 else()
-  set(ORTHANC_FRAMEWORK_DEFAULT_VERSION "1.9.7")
+  set(ORTHANC_FRAMEWORK_DEFAULT_VERSION "1.10.1")
   set(ORTHANC_FRAMEWORK_DEFAULT_SOURCE "web")
 endif()

98070f603b14f10792d65001af0c16fef9b76aeb
Sebastien Jodogne <s.jodogne@gmail.com>
2022-10-28 16:23:18 +0200

    added ViewportLocker

---
+++ OrthancStone/Sources/Viewport/ViewportLocker.cpp
@@ -0,0 +1,51 @@
+/**
+ * Stone of Orthanc
+ * Copyright (C) 2012-2016 Sebastien Jodogne, Medical Physics
+ * Department, University Hospital of Liege, Belgium
+ * Copyright (C) 2017-2022 Osimis S.A., Belgium
+ * Copyright (C) 2021-2022 Sebastien Jodogne, ICTEAM UCLouvain, Belgium
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ **/
+
+
+#include "ViewportLocker.h"
+
+#include <OrthancException.h>
+
+namespace OrthancStone
+{
+  IViewport::ILock& ViewportLocker::GetLock() const
+  {
+    if (lock2_.get() == NULL)
+    {
+      throw Orthanc::OrthancException(Orthanc::ErrorCode_BadSequenceOfCalls);
+    }
+    else
+    {
+      return *lock2_;
+    }
+  }
+  
+
+  ViewportLocker::ViewportLocker(boost::weak_ptr<IViewport> viewport)
+  {
+    lock1_ = viewport.lock();
+    if (lock1_)
+    {
+      lock2_.reset(lock1_->Lock());
+    }
+  }
+}

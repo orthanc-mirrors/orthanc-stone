1fed95a5d8928a3b1e4f436fdd70982e32761d5e
Sebastien Jodogne <s.jodogne@gmail.com>
2026-09-22 18:30:40 +0200

    fix
    --HG--
    branch : refactoring

--- Applications/StoneWebViewer/Plugin/Plugin.cpp
+++ Applications/StoneWebViewer/Plugin/Plugin.cpp
@@ -261,7 +261,11 @@ extern "C"
   {
     OrthancPlugins::SetGlobalContext(context);
 
-    OrthancStone::StoneInitialize(context);
+#if ORTHANC_FRAMEWORK_VERSION_IS_ABOVE(1, 7, 2)
+    Orthanc::Logging::InitializePluginContext(context);
+#else
+    Orthanc::Logging::Initialize(context);
+#endif
 
     /* Check the version of the Orthanc core */
     if (OrthancPluginCheckVersion(context) == 0)
